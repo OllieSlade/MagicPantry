a2d365118b3de906c73ca85864636a0825d9f4d9
Ollie <44728079+OliMations@users.noreply.github.com>
2025-06-14 18:31:50 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,24 @@ import Header from './components/sections/header'
 import Sidebar from './components/sections/sidebar'
 import Results from './components/sections/results'
 
+
 function processData(ingredientsList) {
   return ingredientsList.filter(ingredient => ingredient.include).map(ingredient => ingredient.ingredient);
 }
 
-async function generate_recipe(ingredients) {
-  fetch(`https://magicpantry.netlify.app/.netlify/functions/gemini`, {
+function generate_recipe(ingredients) {
+  return fetch(`https://magicpantry.netlify.app/.netlify/functions/gemini`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({ ingredients })
-  }).then(response => {
-    return response.text();
-  }).catch(error => {
-    console.error("Error generating recipe:", error);
-    return "Error generating recipe. Please try again.";
-  });
+  })
+    .then(response => response.text())
+    .catch(error => {
+      console.error("Error generating recipe:", error);
+      return "Error generating recipe. Please try again.";
+    });
 }
 
 function App() {
@@ -28,10 +29,10 @@ function App() {
   const [APIrecipe, setAPIRecipe] = useState([]);
 
   function refresh() {
-    const generator = generate_recipe(processData(ingredientsList));
-    generator.then((result) => {
+    generate_recipe(processData(ingredientsList)).then((result) => {
+      console.log("Generated Recipe:", result);
       setAIRecipe(result);
-    })
+    });
     // getRecipiesST(processData(ingredientsList)).then((result) => {
     //   setAPIRecipe(result);
     //   console.log("API Recipes:", result);
